1fff94979dd46db0ca0933a5746c3468ccbee59c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 22:25:43 +0000

    Deploy sreenathkrishnan/martian-rust to github.com/sreenathkrishnan/martian-rust.git:gh-pages

--- doc/implementors/serde/de/trait.Error.js
+++ doc/implementors/serde/de/trait.Error.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["docopt"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"docopt/enum.Error.html\" title=\"enum docopt::Error\">Error</a>",synthetic:false,types:["docopt::dopt::Error"]},];
 implementors["serde"] = [];
 implementors["serde_json"] = [{text:"impl <a class=\"trait\" href=\"serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>",synthetic:false,types:["serde_json::error::Error"]},];
 
